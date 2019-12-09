802312b152882a7393c414701ac57116118b0ab8
mike-maher <mike.maher@aqmetrics.com>
2019-12-09 14:17:54 +0000

    [UPDATE] added contact context with dummy data

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import ContactState from './context/contact/ContactState';
 import { Home, About } from './components/pages';
 import Navbar from './components/layout/Navbar';
 import './App.css';
 
 const App = () => {
   return (
-    <Router>
-      <Navbar />
-      <div className="container">
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/about" component={About} />
-        </Switch>
-      </div>
-    </Router>
+    <ContactState>
+      <Router>
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+          </Switch>
+        </div>
+      </Router>
+    </ContactState>
   );
 };
 
---
+++ client/src/context/contact/ContactState.js
@@ -0,0 +1,57 @@
+import React, { useReducer } from 'react';
+import uuid from 'uuid';
+import contactContext from './contactContext';
+import contactReducer from './contactReducer';
+
+import {
+  ADD_CONTACT,
+  UPDATE_CONTACT,
+  SET_ALERT,
+  SET_CURRENT,
+  REMOVE_ALERT,
+  CLEAR_CURRENT,
+  FILTER_CONTACT,
+  CLEAR_FILTER,
+  DELETE_CONTACT
+} from '../types';
+
+const ContactState = props => {
+  const initialState = {
+    contacts: [
+      {
+        id: 1,
+        name: 'Jill Johnson',
+        phone: '111-111-1111',
+        type: 'personal'
+      },
+      {
+        id: 2,
+        name: 'Sara Watson',
+        phone: '222-222-2222',
+        type: 'personal'
+      },
+      {
+        id: 3,
+        name: 'Harry White',
+        phone: '333-333-3333',
+        type: 'professional'
+      }
+    ]
+  };
+
+  const [state, dispatch] = useReducer(contactReducer, initialState);
+
+  // TODO: actions
+
+  return (
+    <contactContext.Provider
+      value={{
+        contacts: state.contacts
+      }}
+    >
+      {props.children}
+    </contactContext.Provider>
+  );
+};
+
+export default ContactState;
