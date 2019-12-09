91818f7461b1043cceb663e9eb1cbdaf519e5833
mike-maher <mike.maher@aqmetrics.com>
2019-12-09 10:02:21 +0000

    [UPDATE] added contact CRUD functions

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator/check');
+const auth = require('../middleware/auth');
+
+const User = require('../models/User');
+const Contact = require('../models/Contact');
 
 /**
  * @route POST api/contacts
  * @desc Get all user's contacts
  * @access PRIVATE
  */
-router.get('/', (req, res) => {
-  res.send('Get users contact');
+router.get('/', auth, async (req, res) => {
+  try {
+    const contacts = await Contact.find({ user: req.user.id }).sort({
+      date: -1
+    });
+    res.json(contacts);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ msg: 'Internal server error' });
+  }
 });
 
 /**
@@ -15,17 +28,71 @@ router.get('/', (req, res) => {
  * @desc add a contact
  * @access PRIVATE
  */
-router.post('/', (req, res) => {
-  res.send('Add contact');
-});
+router.post(
+  '/',
+  [
+    auth,
+    [
+      check('name', 'Name is required')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, phone, type } = req.body;
+    try {
+      const newContact = new Contact({
+        name,
+        email,
+        phone,
+        type,
+        user: req.user.id
+      });
+      const contact = await newContact.save();
+      res.json(contact);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).json({ msg: 'Internal server error' });
+    }
+  }
+);
 
 /**
  * @route POST api/contacts/:id
  * @desc update a contact
  * @access PRIVATE
  */
-router.put('/:id', (req, res) => {
-  res.send('update contact');
+router.put('/:id', auth, async (req, res) => {
+  const { name, email, phone, type } = req.body;
+  const fields = {};
+  if (name) fields.name = name;
+  if (email) fields.email = email;
+  if (phone) fields.phone = phone;
+  if (type) fields.type = type;
+
+  try {
+    let contact = await Contact.findById(req.params.id);
+    if (!contact) {
+      return res.status(404).json({ msg: 'Contact not found' });
+    }
+    if (contact.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'Unauthorized' });
+    }
+    contact = await Contact.findByIdAndUpdate(
+      req.params.id,
+      { $set: fields },
+      { new: true }
+    );
+    res.json(contact);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ msg: 'Internal server error' });
+  }
 });
 
 /**
@@ -33,8 +100,21 @@ router.put('/:id', (req, res) => {
  * @desc delete a contact
  * @access PRIVATE
  */
-router.delete('/:id', (req, res) => {
-  res.send('Delete contact');
+router.delete('/:id', auth, async (req, res) => {
+  try {
+    let contact = await Contact.findById(req.params.id);
+    if (!contact) {
+      return res.status(404).json({ msg: 'Contact not found' });
+    }
+    if (contact.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'Unauthorized' });
+    }
+    await Contact.findByIdAndRemove(req.params.id);
+    res.json({ msg: 'contact removed' });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ msg: 'Internal server error' });
+  }
 });
 
 module.exports = router;
