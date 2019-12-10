91927308c5714240c45f2903ff4010d89c81ac53
mike-maher <mike.maher@aqmetrics.com>
2019-12-10 09:38:30 +0000

    [UPDATE] added alert context and component

--- client/src/App.js
+++ client/src/App.js
@@ -3,27 +3,32 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import ContactState from './context/contact/ContactState';
 import AuthState from './context/auth/AuthState';
 import { Home, About } from './components/pages';
+import AlertState from './context/alert/AlertState';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Alerts from './components/layout/Alerts';
 import Navbar from './components/layout/Navbar';
 import './App.css';
 
 const App = () => {
   return (
     <AuthState>
-      <ContactState>
-        <Router>
-          <Navbar />
-          <div className="container">
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/about" component={About} />
-              <Route exact path="/register" component={Register} />
-              <Route exact path="/login" component={Login} />
-            </Switch>
-          </div>
-        </Router>
-      </ContactState>
+      <AlertState>
+        <ContactState>
+          <Router>
+            <Navbar />
+            <div className="container">
+              <Alerts />
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/about" component={About} />
+                <Route exact path="/register" component={Register} />
+                <Route exact path="/login" component={Login} />
+              </Switch>
+            </div>
+          </Router>
+        </ContactState>
+      </AlertState>
     </AuthState>
   );
 };
